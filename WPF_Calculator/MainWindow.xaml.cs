18fc17964f4d3d6555f7939571eb23ecf9293738
Cirillio <ineed2healing@gmail.com>
2024-10-01 13:11:10 +0700

    Сделал нормально.

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF_Calculator
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
